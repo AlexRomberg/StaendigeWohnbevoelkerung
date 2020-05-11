80e07db8ba3a03d466274d21af1ea34a8c92b323
alexrombi <adze@gmx.ch>
2020-05-11 17:33:26 +0200

    Setup index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
-<?php error_reporting(0); ?>
 <html>
+<?php error_reporting(0); ?>
+
 <head>
-<meta charset="UTF-8">
-OwO
+    <meta charset="UTF-8">
+
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://d3js.org/d3.v5.min.js"></script>
+
+    <title>Wohnbevölkerung</title>
 </head>
