32e3faca95a48b7661374a47744899240dbc801a
Rap22tor <raptorf202@gmail.com>
2020-05-12 13:45:14 +0200

    Added legend

---
+++ src/js/mapfunction.js
@@ -0,0 +1,6 @@
+function community_clicked() {
+    element.onclick = function() {
+        alert('you suck, but that shitty code you wrote did somehow work.');
+        document.getElementById("community").selectedIndex = " Herdern"; // Update dropdown list with the selected community on the map
+    };
+}
